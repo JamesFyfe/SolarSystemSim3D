33778b40d100aeb5a8c94db838ebe300d81563b8
JamesFyfe <jamesfyfe17@gmail.com>
2024-02-22 13:19:44 -0800

    Added Mercury

--- src/CosmicExplorer.js
+++ src/CosmicExplorer.js
@@ -7,6 +7,7 @@ import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPa
 
 const CosmicExplorer = () => {
   const sun = useRef();
+  const mercury = useRef();
   const animationIdRef = useRef(null);
   const composerRef = useRef(null);
   const cameraRef = useRef(null);
@@ -20,10 +21,10 @@ const CosmicExplorer = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     renderer.setPixelRatio(window.devicePixelRatio);
-    
+
     // renderer.setClearColor(new THREE.Color(), 0);
     // renderer.toneMapping = THREE.ReinhardToneMapping;
-    // renderer.toneMappingExposure = 2.5;
+    // renderer.toneMappingExposure = 5;
     // renderer.shadowMap.enabled = true;
     // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     // renderer.antialias = true;
@@ -31,31 +32,41 @@ const CosmicExplorer = () => {
     let previousTimestamp;
 
     // Set up scene
-    camera.position.z = 5;
-    scene.add(new THREE.AmbientLight(0x404040));
+    camera.position.z = 10;
+    scene.add(new THREE.AmbientLight(0xffffff, 0.02));
 
     // Load the sun texture
     const textureLoader = new THREE.TextureLoader();
     const sunTexture = textureLoader.load('/images/star_texture_orange.jpeg');
+    const mercuryTexture = textureLoader.load('/images/mercury_texture.jpeg');
 
-    const geometry = new THREE.SphereGeometry(1, 40, 40);
+    const geometry = new THREE.SphereGeometry(1, 80, 40);
     const material = new THREE.MeshBasicMaterial({
       map: sunTexture,
-      // color: 0x3366ff,
-      // color: 0xdc9250,
-      // color: 0xd08250,
     });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
     sun.current = mesh;
 
-    const sunlight = new THREE.PointLight(0xffffff, 5);
+    const sunlight = new THREE.PointLight(0xffffff, 10);
     sun.current.add(sunlight);
+
+
+    const mercuryGeometry = new THREE.SphereGeometry(0.1, 80, 40);
+    const mercuryMaterial = new THREE.MeshStandardMaterial({
+      map: mercuryTexture,
+    });
+    const mercuryMesh = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
+    scene.add(mercuryMesh);
+    mercuryMesh.position.set(3, 0, 1);
+    mercury.current = mercuryMesh;
 
 
     // Set up bloom effect
     const renderScene = new RenderPass(scene, camera);
     const bloomPass = new UnrealBloomPass(new THREE.Vector2(1, 1), 1, 1, 0.01);
+    const distance = sun.current.position.distanceTo(cameraRef.current.position);
+    bloomPass.strength = 0.3 + distance * 0.06;
     
 
     const composer = new EffectComposer(renderer);
@@ -86,13 +97,7 @@ const CosmicExplorer = () => {
     const handleZoomChange = () => {
       // Example: Adjust bloom parameters based on the camera's distance
       const distance = sun.current.position.distanceTo(cameraRef.current.position);
-      console.log(distance);
-      bloomPass.strength = 0.5 + distance * 0.1; // Adjust as needed
-
-      // Ensure the values are within acceptable ranges
-      bloomPass.radius = THREE.MathUtils.clamp(bloomPass.radius, 0.1, 1.5);
-      bloomPass.threshold = THREE.MathUtils.clamp(bloomPass.threshold, 0.1, 1.0);
-
+      bloomPass.strength = 0.3 + distance * 0.06;
       composerRef.current.render();
     };
 
